4179e400a0853fe2fb1dd02643a4e80c9d2e549c
IKAZE-Divin <ikazedi26@gmail.com>
2026-07-30 16:00:52 +0200

    chore: adding the test all branch to test the homePage

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{html,js}",
+  ],
   theme: {
-    extend: {fontFamily: {
+    extend: {
+      fontFamily: {
         outfit: ['Outfit', 'sans-serif'],
       },
     },
